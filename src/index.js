24431207e99244d857f70fe96c6807e3b824b839
SIEGE\S606553 <vincent.ducrocq@axa.fr>
2017-09-26 18:30:22 +0200

    first component - OK

--- src/index.js
+++ src/index.js
@@ -2,8 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const FirstDiv= () => {
-    return ( <div>Hello boy !!</div> );
+const Title= (props) => {
+    return ( <h1>Welcome {props.name} !</h1> );
 };
+
+class Button extends React.Component {
+    state = { count : 0 };
+
+    // handleClick = () => {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    // };
+
+    handleClick = () => {
+        this.setState((previousState) => {
+            return {
+                count: previousState.count + 1
+            }
+        })
+    };
+
+    render() {
+        return ( <button onClick={this.handleClick}>{this.state.count}</button> );
+    }
+}
   
-ReactDOM.render(<FirstDiv />, root);
+ReactDOM.render(<Title name="John" />, root);
+ReactDOM.render(<Button />, root);
