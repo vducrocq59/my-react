06b7032f0480b35e2b3ebfc3a6ea77c001f9600c
SIEGE\S606553 <vincent.ducrocq@axa.fr>
2017-09-27 16:06:07 +0200

    fin reusble components

--- src/index.js
+++ src/index.js
@@ -3,30 +3,72 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Title= (props) => {
-    return ( <h1>Welcome {props.name} !</h1> );
+    return ( <h1>=> {props.name}</h1> );
 };
 
-class Button extends React.Component {
-    state = { count : 0 };
-
+class ButtonAlone extends React.Component {
+    state = { myCounter: 0 };
+    
+    // // SANS ETAT PRECEDENT
     // handleClick = () => {
-    //     this.setState({
-    //         count: this.state.count + 1
-    //     })
-    // };
+    //     this.setState({ myCounter: this.state.myCounter+1 })
+    // }
 
+    // AVEC ETAT PRECEDENT
     handleClick = () => {
         this.setState((previousState) => {
-            return {
-                count: previousState.count + 1
-            }
+            return { myCounter: previousState.myCounter+1 };
         })
-    };
+    }
 
     render() {
-        return ( <button onClick={this.handleClick}>{this.state.count}</button> );
+        return(<button onClick={ this.handleClick } >Bouton non réutilisable => { this.state.myCounter }</button>)
     }
 }
+
+class Button extends React.Component {
+    render() {
+        return ( <button onClick={ () => this.props.onClickFunction(this.props.incrementBy)}>+ {this.props.incrementBy}</button> );
+    }
+}
+
+const Result = (props) => {
+    return (
+        <div>Mon résultat => {props.result}</div>
+    );
+};
+
+class App extends React.Component {
+    state = { count : 0 };
+    
+    incrementCount = (incrementBy) => {
+        this.setState((previousState) => ({
+                count: previousState.count + incrementBy
+            }));
+        };
+
+    render() {
+        return (
+            <div>
+                <Title name="Bouton non réutilisable" />
+                <ButtonAlone />
+
+                <Title name="Bouton réutilisable" />
+                <Button incrementBy={1} onClickFunction={this.incrementCount}  />
+                <Button incrementBy={5} onClickFunction={this.incrementCount}  />
+                <Button incrementBy={10} onClickFunction={this.incrementCount}  />
+                <Button incrementBy={100} onClickFunction={this.incrementCount}  />
+                <Result result={this.state.count} />
+            </div>
+        );
+    };
+}
   
-ReactDOM.render(<Title name="John" />, root);
-ReactDOM.render(<Button />, root);
+// Test 1 => élément statique
+// ReactDOM.render(<Title name="John" />, root);
+
+// Test 2 => Bouton avec changement de statut
+// ReactDOM.render(<Button />, root);
+
+// Test 3 => Composant réutilisable
+ReactDOM.render(<App />, root);
