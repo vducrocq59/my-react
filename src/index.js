5c84b611bc873c7aa771f31f3ad6576a5cc2e99b
SIEGE\S606553 <vincent.ducrocq@axa.fr>
2017-09-27 21:45:10 +0200

    feat: reusable card

--- src/index.js
+++ src/index.js
@@ -2,15 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <div>
-            <img alt="Il est joli" src="" />
-            <div>My name...</div>
+        <div style={{margin: '1em'}}>
+            <img width="75" alt=":'(" src={props.avatarUrl} />
+            <div style={{display: 'inline-block', marginLeft: 10}}>
+                <div style={{fontSize: '1.25em', fontWeight: 'bold'}} className="info">{props.name}</div>
+                <div style={{fontSize: '1em'}} className="info">{props.company}</div>
+            </div>
         </div>
     ) ;
 }
 
-// const CardList = ...
+let data = [
+    { 
+        name:"Paul O’Shannessy", 
+        company:"facebook", 
+        avatarUrl:"https://avatars1.githubusercontent.com/u/8445?v=4" 
+    },
+    { 
+        name:"Paul O’Shannessy", 
+        company:"facebook", 
+        avatarUrl:"https://avatars1.githubusercontent.com/u/8445?v=4" 
+    }
+];
+
+const CardList = (props) => {
+    return (
+        <div>
+            { props.guys.map(guy => <Card {...guy} />) }
+        </div>
+    );
+}
   
-ReactDOM.render(<Card />, root);
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <CardList />
+//             </div>
+//         );
+//     }
+// }
+
+ReactDOM.render(<CardList guys={data} />, root);
