90c2b344f6169f068715d07a8899ec05ead7ad56
SIEGE\S606553 <vincent.ducrocq@axa.fr>
2017-09-27 19:07:51 +0200

    tmp

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const FirstDiv= () => {
-    return ( <div>Hello boy !!</div> );
-};
+const Card = () => {
+    return (
+        <div>
+            <img alt="Il est joli" src="" />
+            <div>My name...</div>
+        </div>
+    ) ;
+}
+
+// const CardList = ...
   
-ReactDOM.render(<FirstDiv />, root);
+ReactDOM.render(<Card />, root);
